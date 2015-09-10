be677d1eff02eeb8b177ca10119c23d1d844c35c
Nate Wright <natew@notthisway.com>
2015-09-10 15:37:41 +0100

    Mockup participants/files/messages panels in task modal

--- assets/js/task-workflow.js
+++ assets/js/task-workflow.js
@@ -6,7 +6,8 @@ $(document).ready(function ($) {
 	var tasks = $( '.task-list .tasks > li' );
 	var modals = $( '.pkp_modal' );
 
-	tasks.click( function() {
+	tasks.click( function(e) {
+		e.preventDefault();
 		task_open_task( $(this).data('task') );
 	});
 
@@ -24,19 +25,44 @@ $(document).ready(function ($) {
 		}
 	});
 
+	// Register tab listeners
+	modals.find( '.panels .tabs a' ).on('click', function(e) {
+		e.preventDefault();
+		var modal = $(this).parents( '.pkp_modal' );
+		task_select_tab( modal, $(this).data('target') );
+	});
+
+	// Open a task and initialize it
 	function task_open_task( task ) {
 		var modal = $('#' + task );
 		if ( !modal.length ) {
 			alert( 'No example available yet.' );
+			return;
 		}
 
 		modal.addClass('is_visible');
 		body.addClass('modal_is_visible');
+		task_select_tab( modal, 'participants' );
 	}
 
+	// Close a task and clear listeners
 	function task_close_task() {
 		$('.pkp_modal').removeClass('is_visible');
-		body.removeClass('is_visible');
+		body.removeClass('modal_is_visible');
+	}
+
+	// Select a tab in a task
+	function task_select_tab( modal, tab ) {
+		var tabs = modal.find('.panels .tabs > li');
+		tabs = tabs.add( modal.find('.panel') );
+
+		tabs.removeClass('current');
+
+		tabs.each(function() {
+			if ( $(this).hasClass( tab ) ) {
+				$(this).addClass('current');
+			}
+		});
 	}
 
 });
